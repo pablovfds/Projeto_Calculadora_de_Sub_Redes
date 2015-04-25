7849037a646cafd10e5d3cb41edfb1dfe360f64c
Páblo Victor <pablo.santos@ccc.ufcg.edu.br>
2015-04-25 17:35:18 -0300

    Criação de uma nova função para calculo de rotas e mudanças na formatação

--- calculadoraDeSubRedes.py
+++ calculadoraDeSubRedes.py
@@ -1,16 +1,16 @@
 __author__ = 'PabloVictor'
-
 #coding: utf-8
 
 import math
+import random
 
 def leituraDoEnderecoIP():
-    erroNaEntradaDeDados = True
+    erro = True
 
-    while erroNaEntradaDeDados:
-        erroNaEntradaDeDados=False
+    while erro:
+        erro=False
         try:
-            enderecoIP = raw_input('Insira o endereco IP: ')
+            enderecoIP = raw_input('Insira o endereco IP(Ex: 192.167.0.23): ')
 
             enderecoIPAux = enderecoIP.split(".")
 
@@ -19,95 +19,99 @@ def leituraDoEnderecoIP():
             else:
                 for parteDoIP in enderecoIPAux:
                     if int(parteDoIP) > 192 or int(parteDoIP) < 0:
-                        erroNaEntradaDeDados=True
-
-                if(erroNaEntradaDeDados):
-                    print "Endereco IP invalido."
+                        erro=True
+                        print "Endereco IP invalido."
         except ValueError:
-            erroNaEntradaDeDados = True
+            erro = True
             print("Endereco IP invalido.")
 
     return enderecoIP
 
 def leituraDoNumeroDeSubRedes(mascaraOriginal):
-    erroNaEntradaDeDados = True
+    erro = True
 
-    numeroMaximoDeSubRedes = math.pow(2,32 - mascaraOriginal ) #calcula o numero maximo de sub redes disponiveis
+    numeroMaximoDeSubRedes = math.pow(2,32 - mascaraOriginal )
 
-    while erroNaEntradaDeDados:
-        erroNaEntradaDeDados = False
+    while erro:
+        erro = False
         try:
-            numeroDeSubRedes = int(raw_input("Insira o numero de sub-redes: "))
+            numeroDeSubRedes = int(raw_input("Insira o numero de sub-redes(Ex: 4): "))
 
             if(numeroDeSubRedes < 0 or numeroDeSubRedes > numeroMaximoDeSubRedes):
-                erroNaEntradaDeDados = True
+                erro = True
                 print "Numero de sub-redes invalido."
         except ValueError:
-            erroNaEntradaDeDados = True
+            erro = True
             print "Numero de sub-redes invalido."
 
     return numeroDeSubRedes
 
 def leituraDaMascaraOriginal():
-    erronaentradadedados = True
+    erro = True
 
-    while erronaentradadedados:
-        erronaentradadedados=False
+    while erro:
+        erro=False
         try:
-            mascaraOriginal = int(raw_input('Insira a mascara original da rede: '))
+            mascaraOriginal = raw_input('Insira a mascara original da rede (Ex: 255.255.255.0): ')
+            listaMascara = mascaraOriginal.split(".")
 
-            if(mascaraOriginal >= 30  and mascaraOriginal < 8):
-                erronaentradadedados = True
+            if(len(listaMascara) != 4):
+                erro = True
                 print("Mascara invalida.Digite uma nova mascara.")
+
+            mascaraOriginalAux = ""
+            for index in range(len(listaMascara)):
+                if(int(listaMascara[index]) > 255 or int(listaMascara[index]) < 0):
+                    erro = True
+                    print("Mascara invalida.Digite uma nova mascara.")
+                elif (index > 0):
+                    if (int(listaMascara[index]) > int(listaMascara[index-1]) or int(listaMascara[index-1] < 255)):
+                        erro = True
+                        print("Mascara invalida.Digite uma nova mascara.")
+
+                binarioAux = (bin(int(listaMascara[index])))[2:]
+                mascaraOriginalAux += ((8-len(binarioAux))*"0") + binarioAux
+
+            numeroDeMascara = 0
+            for index in range(32):
+                if(mascaraOriginalAux[index] == "1"):
+                    numeroDeMascara+=1
+
         except ValueError:
-            erronaentradadedados = True
+            erro = True
             print("Mascara invalida.Digite uma nova mascara.")
 
-    return mascaraOriginal
+    return numeroDeMascara
 
 def verificaClasseDoEnderecoIP(enderecoIPOriginal):
-    msgResposta = ""
-
     lista = enderecoIPOriginal.split(".")
 
-    if(int(lista[0]) < 128 and int(lista[0]) >= 10): msgResposta = "A"
-    elif(int(lista[0]) < 192 and int(lista[0]) > 127): msgResposta = "B"
-    elif(int(lista[0]) < 224 and int(lista[0]) > 191): msgResposta = "C"
-
-    return msgResposta
+    if(int(lista[0]) < 128 and int(lista[0]) >= 10): print("A classe do endereço IP "+enderecoIPOriginal+ ": A")
+    elif(int(lista[0]) < 192 and int(lista[0]) > 127): print("A classe do endereço IP "+enderecoIPOriginal+ ": B")
+    elif(int(lista[0]) < 224 and int(lista[0]) > 191): print("A classe do endereço IP "+enderecoIPOriginal+ ": C")
 
 def verificaRedePrivada(enderecoIP):
     enderecoIPLista = enderecoIP.split(".")
     if (int(enderecoIPLista[0]) == 10) or (int(enderecoIPLista[0]) == 172 and int(enderecoIPLista[1]) >= 16 and int(enderecoIPLista[1]) <= 31) or (int(enderecoIPLista[0]) == 192 and int(enderecoIPLista[1]) >= 168):
-        msgResposta = "pertence"
-    else: msgResposta = "nao pertence"
-    return msgResposta
+        print("O endereço IP "+enderecoIP+" é uma rede privada: sim")
+    else: print("O endereço IP "+enderecoIP+" é uma rede privada: não")
 
-def calculaNovaMascara(numeroDeSubRedes, mascaraOriginal): return (calculaQuantidadeDeBits(numeroDeSubRedes) + int(mascaraOriginal))
+def calculaNovaMascara(numeroDeSubRedes, mascaraOriginal):
+    novaMascara = calculaQuantidadeDeBits(numeroDeSubRedes) + int(mascaraOriginal)
+    binarioDeNovaMascara = transformaParaFormaIp("1"*novaMascara + (32-novaMascara)*"0")
+    return (binarioDeNovaMascara + "/"+str(novaMascara))
 
 def calculaQuantidadeDeBits(numeroDeSubRedes): return int(math.ceil(math.log(numeroDeSubRedes)/math.log(2)))
 
-def transformaEnderecoIpParaBinario(enderecoIP):
+def transformaEnderecoParaBinario(endereco):
     listaBinariaDoEnderecoIP = []
 
-    for parteDoIP in enderecoIP.split("."):
+    for parteDoIP in endereco.split("."):
         octetoAux = bin(int(parteDoIP))[2:]
         octetoAux = (8-len(octetoAux))*"0" + octetoAux
         listaBinariaDoEnderecoIP.append(octetoAux)
 
     return "".join(listaBinariaDoEnderecoIP)
-
-def calculaNovoOctetoEmBinario(posicaoDoOcteto, enderecoIP):
-    listaBinariaDoEnderecoIP = transformaEnderecoIpParaBinario(enderecoIP)
-    numeroDeBits = calculaQuantidadeDeBits(numeroDeSubRedes)
-    listaBinariaDoEnderecoIPNovoOcteto = []
-
-    for octeto in range(numeroDeSubRedes):
-        novoOcteto = bin(octeto)[2:] + listaBinariaDoEnderecoIP[posicaoDoOcteto][numeroDeBits:]
-        novoOcteto = (8-len(novoOcteto))*"0" + novoOcteto
-        listaBinariaDoEnderecoIPNovoOcteto.append(novoOcteto)
-
-    return listaBinariaDoEnderecoIPNovoOcteto
 
 def transformaParaFormaIp(listaIP):
     listaIPAux = []
@@ -118,27 +122,55 @@ def transformaParaFormaIp(listaIP):
 
     return ".".join(listaIPAux)
 
-def transformaBinarioEmDecimal(ipBinario):
-    return str(int(ipBinario, 2))
+def transformaBinarioEmDecimal(ipBinario): return str(int(ipBinario, 2))
 
 def calculaSubRedes(enderecoIPOriginal, mascaraOriginal, numeroDeSubRedes):
-    binarioDoIP = transformaEnderecoIpParaBinario(enderecoIPOriginal)
+    binarioDoIP = transformaEnderecoParaBinario(enderecoIPOriginal)
     ipOriginal = binarioDoIP[:mascaraOriginal]
+    listaDeIpDeRede = []
 
-    print("Endereco IP         - Forma Binaria                    - Forma Decimal")
+    formatacao = '{0:10s} {1:10s} {2:20s} {3:39s} {4:15s}'
+    formatacaoTitulo = '{0:10s} {1:10s} {2:21s} {3:39s} {4:15s}'
+    print(formatacaoTitulo.format("","Sub Rede", "Endereço IP", "Forma Binaria", "Forma Decimal"))
     for i in range(numeroDeSubRedes):
-        a = (calculaQuantidadeDeBits(numeroDeSubRedes)-len(bin(i)[2:]))*"0" + bin(i)[2:]
+        aux = (calculaQuantidadeDeBits(numeroDeSubRedes)-len(bin(i)[2:]))*"0" + bin(i)[2:]
 
-        ipR = ipOriginal + a + (32-(len(ipOriginal)+len(a)))*"0"
-        ipB = ipOriginal + a + (32-(len(ipOriginal)+len(a)))*"1"
-        print(str(i+1) + " - Sub rede")
-        print("Inicial: " + transformaParaFormaIp(ipR) + " - " + ipR + " - " + transformaBinarioEmDecimal(ipR))
-        print("Final: " + transformaParaFormaIp(ipB) + " - " + ipB + " - " + transformaBinarioEmDecimal(ipB))
+        ipR = ipOriginal + aux + (32-(len(ipOriginal)+len(aux)))*"0"
+        ipB = ipOriginal + aux + (32-(len(ipOriginal)+len(aux)))*"1"
+        formaIPRede = transformaParaFormaIp(ipR)
+        formaIPBroadCast = transformaParaFormaIp(ipB)
+        listaDeIpDeRede.append(formaIPRede)
+        subRede = str(i+1)
+        print(formatacao.format("Inicial",subRede, formaIPRede, ipR, transformaBinarioEmDecimal(ipR)))
+        print(formatacao.format("Final",subRede, formaIPBroadCast, ipB, transformaBinarioEmDecimal(ipB)))
 
+
+
+    calculaRotas(listaDeIpDeRede, numeroDeSubRedes, mascaraOriginal)
+
+def calculaRotas(listaDeIpDeRede, numeroDeSubRedes, mascaraOriginal):
+    print
+
+    formatacao = '{0:16s} {1:16s} {2:19s} {3:7s} {4:10s}'
+    formatacaoTitulo = '{0:16s} {1:16s} {2:19s} {3:7s} {4:10s}'
+
+    print(formatacaoTitulo.format("Destinion", "Gateway", "Mascara", "Option", "Interface"))
+    for index in range(len(listaDeIpDeRede)):
+        ipRedeAtual = listaDeIpDeRede[index][:-1] + str (int(listaDeIpDeRede[index][-1])+1)
+        novaMascara = calculaNovaMascara(numeroDeSubRedes, mascaraOriginal)
+        if index == 0:
+            print(formatacao.format("0.0.0.0", ipRedeAtual, "0.0.0.0", "UG", "ETH"+str(index)))
+            print(formatacao.format(listaDeIpDeRede[index], "0.0.0.0", novaMascara, "U", "ETH"+str(index)))
+        else:
+            print(formatacao.format(listaDeIpDeRede[index], ipRedeAtual, novaMascara, "U", "ETH"+str(index)))
 #-------Main--------
 
 enderecoIPOriginal = leituraDoEnderecoIP()
 mascaraOriginal = leituraDaMascaraOriginal()
 numeroDeSubRedes = leituraDoNumeroDeSubRedes(mascaraOriginal)
 
+verificaClasseDoEnderecoIP(enderecoIPOriginal)
+verificaRedePrivada(enderecoIPOriginal)
+print("Nova mascara: " + calculaNovaMascara(numeroDeSubRedes, mascaraOriginal))
+print
 calculaSubRedes(enderecoIPOriginal, mascaraOriginal, numeroDeSubRedes)
